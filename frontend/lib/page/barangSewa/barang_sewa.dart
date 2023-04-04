8fab0aa441532f95f909806b05e3de71a6ba9581
AgilS121 <gilsatria121@gmail.com>
2023-04-05 05:49:09 +0700

    update halaman 'barang sewa' , 'edit/tambah barang' - agils

--- frontend/lib/page/barangSewa/barang_sewa.dart
+++ frontend/lib/page/barangSewa/barang_sewa.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/models/barang.dart';
+import 'package:frontend/page/updateTambahBarang/update_tambah_barang.dart';
 import 'package:frontend/theme/pallete.dart';
 import 'package:intl/intl.dart';
 
@@ -35,123 +36,146 @@ class _BarangSewaState extends State<BarangSewa> {
 
   body() {
     return Padding(
-      padding: EdgeInsets.all(20.0),
-      child: ListView.builder(
-        itemCount: barang.length,
-        itemBuilder: (BuildContext context, int index) {
-          if (barang[index].status != 'Selesai') {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                  width: 355,
-                  height: 90,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.grey.withOpacity(0.5),
-                        spreadRadius: 2,
-                        blurRadius: 5,
-                        offset: Offset(0, 3),
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: barang.length,
+              itemBuilder: (BuildContext context, int index) {
+                if (barang[index].status != 'Selesai') {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Container(
+                        width: 355,
+                        height: 90,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                              offset: Offset(0, 3),
+                            ),
+                          ],
+                        ),
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(vertical: 8),
+                          child: ListTile(
+                            leading: Image.network(
+                              barang[index].gambar,
+                              width: 94,
+                              height: 94,
+                              fit: BoxFit.cover,
+                            ),
+                            title: Text(barang[index].namaBarang),
+                            subtitle: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  barang[index].namaToko,
+                                  style: TextStyle(
+                                    fontSize: 10,
+                                    fontWeight: FontWeight.w700,
+                                  ),
+                                ),
+                                Text(
+                                  barang[index].kategori,
+                                  style: TextStyle(
+                                    fontSize: 9,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  formatCurrency
+                                      .format(barang[index].hargaBarang),
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w600,
+                                      color: MyColors.bg),
+                                ),
+                              ],
+                            ),
+                            trailing: GestureDetector(
+                              child: Icon(Icons.drag_handle),
+                              onTapDown: (details) {
+                                showMenu(
+                                  context: context,
+                                  position: RelativeRect.fromLTRB(
+                                    details.globalPosition.dx,
+                                    details.globalPosition.dy,
+                                    0,
+                                    0,
+                                  ),
+                                  items: [
+                                    PopupMenuItem(
+                                      child: Row(
+                                        children: [
+                                          Icon(Icons.edit, color: Colors.blue),
+                                          SizedBox(width: 5),
+                                          Text('Edit',
+                                              style: TextStyle(
+                                                  color: Colors.blue)),
+                                        ],
+                                      ),
+                                      onTap: () {
+                                        // Tambahkan kode untuk aksi edit barang
+                                      },
+                                    ),
+                                    PopupMenuItem(
+                                      child: Row(
+                                        children: [
+                                          Icon(Icons.delete, color: Colors.red),
+                                          SizedBox(width: 5),
+                                          Text('Delete',
+                                              style:
+                                                  TextStyle(color: Colors.red)),
+                                        ],
+                                      ),
+                                      onTap: () {
+                                        // Tambahkan kode untuk aksi hapus barang
+                                      },
+                                    ),
+                                  ],
+                                );
+                              },
+                            ),
+                          ),
+                        ),
                       ),
+                      SizedBox(
+                        height: 20,
+                      )
                     ],
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 8),
-                    child: ListTile(
-                      leading: Image.network(
-                        barang[index].gambar,
-                        width: 94,
-                        height: 94,
-                        fit: BoxFit.cover,
-                      ),
-                      title: Text(barang[index].namaBarang),
-                      subtitle: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            barang[index].namaToko,
-                            style: TextStyle(
-                              fontSize: 10,
-                              fontWeight: FontWeight.w700,
-                            ),
-                          ),
-                          Text(
-                            barang[index].kategori,
-                            style: TextStyle(
-                              fontSize: 9,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            formatCurrency.format(barang[index].hargaBarang),
-                            style: TextStyle(
-                                fontSize: 10,
-                                fontWeight: FontWeight.w600,
-                                color: MyColors.bg),
-                          ),
-                        ],
-                      ),
-                      trailing: GestureDetector(
-                        child: Icon(Icons.drag_handle),
-                        onTapDown: (details) {
-                          showMenu(
-                            context: context,
-                            position: RelativeRect.fromLTRB(
-                              details.globalPosition.dx,
-                              details.globalPosition.dy,
-                              0,
-                              0,
-                            ),
-                            items: [
-                              PopupMenuItem(
-                                child: Row(
-                                  children: [
-                                    Icon(Icons.edit, color: Colors.blue),
-                                    SizedBox(width: 5),
-                                    Text('Edit',
-                                        style: TextStyle(color: Colors.blue)),
-                                  ],
-                                ),
-                                onTap: () {
-                                  // Tambahkan kode untuk aksi edit barang
-                                },
-                              ),
-                              PopupMenuItem(
-                                child: Row(
-                                  children: [
-                                    Icon(Icons.delete, color: Colors.red),
-                                    SizedBox(width: 5),
-                                    Text('Delete',
-                                        style: TextStyle(color: Colors.red)),
-                                  ],
-                                ),
-                                onTap: () {
-                                  // Tambahkan kode untuk aksi hapus barang
-                                },
-                              ),
-                            ],
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                )
-              ],
-            );
-          } else {
-            return SizedBox.shrink();
-          }
-        },
+                  );
+                } else {
+                  return SizedBox.shrink();
+                }
+              },
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => updatetambahBarang()));
+              },
+              child: Icon(Icons.add),
+              backgroundColor: MyColors.bg,
+            ),
+          )
+        ],
       ),
     );
   }
