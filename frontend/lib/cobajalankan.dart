286900c0362fd3afe2ccbf537ca697d5581acf23
Agils121 <gilsatria121@gmail.com>
2023-06-19 19:05:22 +0700

    cobadeh

---
+++ frontend/lib/cobajalankan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CobaJalankan extends StatefulWidget {
+  const CobaJalankan({super.key});
+
+  @override
+  State<CobaJalankan> createState() => _CobaJalankanState();
+}
+
+class _CobaJalankanState extends State<CobaJalankan> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
